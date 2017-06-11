051273235d0d9f6dcbeb300fb115a9ad14a4fc6b
Alexander <abkolodiychuk@gmail.com>
2017-06-11 15:32:47 +0200

    Animation transitions on triggers and parameters

--- Assets/Scripts/Actors/Attacker.cs
+++ Assets/Scripts/Actors/Attacker.cs
@@ -7,13 +7,31 @@ namespace Actors {
     [Range(-2.0f, 5.0f)]
     public float Speed;
 
+    private float _currentSpeed;
+
+    private void Start() {
+      _currentSpeed = Speed;
+    }
+
     private void Update() {
-      transform.Translate(Vector3.left * Speed * Time.deltaTime);
+      transform.Translate(Vector3.left * _currentSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
       Debug.Log(gameObject.name + " touched " + other.gameObject.name);
     }
+
+    public void SetSpeed(float speed) {
+      _currentSpeed = speed;
+    }
+
+    public void ResetDefaultSpeed() {
+      _currentSpeed = Speed;
+    }
+
+    public void StrikeCurrentTarget(float damage) {
+      Debug.Log(name + " hit target with damage " + damage);
+    } 
 
   }
 }
