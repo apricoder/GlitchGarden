c53cc1ac8044288c622445ff6f1262c14461144e
Alexander <abkolodiychuk@gmail.com>
2017-06-13 00:17:45 +0200

    Better shooting defenders prefabs, shooting projectiles from animation events

---
+++ Assets/Scripts/Actors/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Actors {
+
+  public class Shooter : MonoBehaviour {
+
+    public GameObject Gun;
+    public GameObject Projectile;
+    public GameObject ProjectilesParent;
+
+    public void Shoot() {
+      Instantiate(Projectile, Gun.transform.position, Quaternion.identity, ProjectilesParent.transform);
+    }
+    
+  }
+}
